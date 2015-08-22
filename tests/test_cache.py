909c69741e08a740ec7a005ed6f56c7802e9cef0
Ricardo Kirkner <ricardo@kirkner.com.ar>
2015-08-22 13:14:05 -0300

    add support for removing metadata for a virtualenv from the cache index
    ensure access to the cache index is protected by an exclusive lock to
    avoid multiple processes causing race conditions while the cache file
    is being rewritten.

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -19,8 +19,9 @@
 import json
 import os
 import tempfile
+import time
 import unittest
-
+from threading import Thread
 from unittest.mock import patch
 
 from pkg_resources import parse_requirements
@@ -103,6 +104,83 @@ class StoreTestCase(TempfileTestCase):
             self.assertEqual(data['installed'], 'installed')
             self.assertEqual(data['metadata'], 'metadata')
             self.assertEqual(data['interpreter'], 'interpreter')
+
+
+class RemoveTestCase(TempfileTestCase):
+    """Remove virtualenv from cache."""
+
+    def test_missing_file(self):
+        venvscache = cache.VEnvsCache(self.tempfile)
+        venvscache.remove('missing/path')
+
+        lines = venvscache._read_cache()
+        self.assertEqual(lines, [])
+
+    def test_missing_env_in_cache(self):
+        venvscache = cache.VEnvsCache(self.tempfile)
+        venvscache.store('installed', {'env_path': 'some/path'}, 'interpreter')
+        lines = venvscache._read_cache()
+        assert len(lines) == 1
+
+        venvscache.remove('some/path')
+
+        lines = venvscache._read_cache()
+        self.assertEqual(lines, [])
+
+    def test_preserve_cache_data_ordering(self):
+        venvscache = cache.VEnvsCache(self.tempfile)
+        # store 3 venvs
+        venvscache.store('installed1', {'env_path': 'path/env1'}, 'interpreter')
+        venvscache.store('installed2', {'env_path': 'path/env2'}, 'interpreter')
+        venvscache.store('installed3', {'env_path': 'path/env3'}, 'interpreter')
+
+        venvscache.remove('path/env2')
+
+        lines = venvscache._read_cache()
+        self.assertEqual(len(lines), 2)
+        self.assertEqual(
+            json.loads(lines[0]).get('metadata').get('env_path'), 'path/env1')
+        self.assertEqual(
+            json.loads(lines[1]).get('metadata').get('env_path'), 'path/env3')
+
+    def test_lock_cache_for_remove(self):
+        venvscache = cache.VEnvsCache(self.tempfile)
+        # store 3 venvs
+        venvscache.store('installed1', {'env_path': 'path/env1'}, 'interpreter')
+        venvscache.store('installed2', {'env_path': 'path/env2'}, 'interpreter')
+        venvscache.store('installed3', {'env_path': 'path/env3'}, 'interpreter')
+
+        # patch _write_cache so it emulates a slow write during which
+        # another process managed to modify the cache file before the
+        # first process finished writing the modified cache data
+        original_write_cache = venvscache._write_cache
+        p = patch('fades.cache.VEnvsCache._write_cache')
+        mock_write_cache = p.start()
+
+        t1 = Thread(target=venvscache.remove, args=('path/env1',))
+
+        def slow_write_cache(*args, **kwargs):
+            p.stop()
+            t1.start()
+            # wait to ensure t1 thread must wait for lock to be released
+            time.sleep(0.01)
+            original_write_cache(*args, **kwargs)
+
+        mock_write_cache.side_effect = slow_write_cache
+
+        # just a sanity check
+        assert not os.path.exists(venvscache.filepath + '.lock')
+        # remove a virtualenv from the cache
+        venvscache.remove('path/env2')
+        t1.join()
+
+        # when cache file is properly locked both virtualenvs
+        # will have been removed from the cache
+        lines = venvscache._read_cache()
+        self.assertEqual(len(lines), 1)
+        self.assertEqual(
+            json.loads(lines[0]).get('metadata').get('env_path'), 'path/env3')
+        self.assertFalse(os.path.exists(venvscache.filepath + '.lock'))
 
 
 class SelectionTestCase(TempfileTestCase):
