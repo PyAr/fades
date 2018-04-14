37e1f0e97354785ed97f63ff5b297214740e2353
Juan Carlos <juan@thirstie.com>
2018-04-13 23:13:28 -0300

    Improve basic test for list_venvs.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -383,8 +383,8 @@ class ListVenvsTestCase(unittest.TestCase):
 
     maxDiff, __slots__ = None, ()
 
-    @unittest.skipIf('TRAVIS' in os.environ,
-                     "Travis weird scaping https://travis-ci.org/PyAr/fades/jobs/366371756#L764")
+    @unittest.skipIf('TRAVIS' in os.environ or 'APPVEYOR' in os.environ,
+                     "Travis/AppVeyor weird scaping https://travis-ci.org/PyAr/fades/jobs/366371756#L764")
     def test_list_venvs(self):
         venvs_idx = os.path.join(PATH_TO_EXAMPLES, 'venvs.idx')
         venvs_info = os.path.join(PATH_TO_EXAMPLES, 'output.txt')
