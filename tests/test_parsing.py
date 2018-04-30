d4fe47ffc020ebd0e70f02e8e51afafdbdc99ccd
Martin Alderete <malderete@gmail.com>
2018-04-29 23:04:14 -0300

    Changed the try-except block with a new case which lookup for "fade" before hashtag "#"
    Changes log level from warning to debug in the case when fades does not understand the import (strange cases)
    Updated tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -292,8 +292,8 @@ class FileParsingTestCase(unittest.TestCase):
         parsed = parsing._parse_content(io.StringIO("""
             from foo bar import :(   # fades
         """))
-        self.assertLoggedWarning("Not understood import info",
-                                 "['from', 'foo', 'bar', 'import', ':(']")
+        self.assertLoggedDebug("Not understood import info",
+                               "['from', 'foo', 'bar', 'import', ':(']")
         self.assertDictEqual(parsed, {})
 
     def test_strange_fadesinfo(self):
@@ -453,20 +453,28 @@ class FileParsingTestCase(unittest.TestCase):
         })
 
     def test_fades_and_hashtag_mentioned_in_code(self):
+        """Test the case where a string contains both: fades and hashtag (#)
+        but is not an import.
+        """
         parsed = parsing._parse_content(io.StringIO("""
           'http://fades.readthedocs.io/en/release-7-0/readme.html#how-to-use-it'
         """))
         self.assertDictEqual(parsed, {})
-        self.assertLoggedDebug('fades and # found but in a code line skipped.')
 
-    def test_fades_and_hashtag_mentioned_in_code_with_imports(self):
+    def test_fades_and_hashtag_mentioned_in_code_mixed_with_imports(self):
         parsed = parsing._parse_content(io.StringIO("""import requests  # fades
 
           'http://fades.readthedocs.io/en/release-7-0/readme.html#how-to-use-it'
         """))
-        self.assertEqual(len(parsed), 1)
-        self.assertIn('pypi', parsed.keys())
-        self.assertLoggedDebug('fades and # found but in a code line skipped.')
+        self.assertDictEqual(parsed, {
+            REPO_PYPI: [get_req('requests')]
+        })
+
+    def test_fades_user_strange_comment_with_hashtag_ignored(self):
+        parsed = parsing._parse_content(io.StringIO("""
+          import foo # fades==2 # Some comment with #hashtash
+        """))  # noqa
+        self.assertDictEqual(parsed, {})
 
 
 class ManualParsingTestCase(unittest.TestCase):
