47ed4626e52e0674191ecbe795f5de4eb9d0ca87
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2019-05-23 22:12:48 +0200

    Refactor test infra

--- tests/test_infra.py
+++ tests/test_infra.py
@@ -18,17 +18,14 @@
 
 import io
 import logging
-import os
-import unittest
-
-from unittest.mock import patch
-
 import docutils.core
 import pep257
 import rst2html5_
 
 from flake8.api.legacy import get_style_guide
 from pyuca import Collator
+
+from tests.conftest import _get_python_filepaths
 
 FLAKE8_ROOTS = ['fades', 'tests']
 FLAKE8_OPTIONS = ['--max-line-length=99', '--select=E,W,F,C,N']
@@ -40,44 +37,35 @@ for logger_name in ('flake8.plugins', 'flake8.api', 'flake8.checker', 'flake8.ma
     logging.getLogger(logger_name).setLevel(logging.CRITICAL)
 
 
-class InfrastructureTestCase(unittest.TestCase):
+def test_flake8_pytest(mocker):
+    python_filepaths = _get_python_filepaths(FLAKE8_ROOTS)
+    style_guide = get_style_guide(paths=FLAKE8_OPTIONS)
+    fake_stdout = io.StringIO()
+    mocker.patch('sys.stdout', fake_stdout)
+    report = style_guide.check_files(python_filepaths)
+    assert report.total_errors == 0, "There are issues!\n" + fake_stdout.getvalue()
 
-    def _get_python_filepaths(self, roots):
-        """Helper to retrieve paths of Python files."""
-        python_paths = []
-        for root in roots:
-            for dirpath, dirnames, filenames in os.walk(root):
-                for filename in filenames:
-                    if filename.endswith(".py"):
-                        python_paths.append(os.path.join(dirpath, filename))
-        return python_paths
 
-    def test_flake8(self):
-        python_filepaths = self._get_python_filepaths(FLAKE8_ROOTS)
-        style_guide = get_style_guide(paths=FLAKE8_OPTIONS)
-        fake_stdout = io.StringIO()
-        with patch('sys.stdout', fake_stdout):
-            report = style_guide.check_files(python_filepaths)
-        self.assertEqual(report.total_errors, 0, "There are issues!\n" + fake_stdout.getvalue())
+def test_pep257_pytest():
+    python_filepaths = _get_python_filepaths(PEP257_ROOTS)
+    result = list(pep257.check(python_filepaths))
+    assert len(result) == 0, "There are issues!\n" + '\n'.join(map(str, result))
 
-    def test_pep257(self):
-        python_filepaths = self._get_python_filepaths(PEP257_ROOTS)
-        result = list(pep257.check(python_filepaths))
-        self.assertEqual(len(result), 0, "There are issues!\n" + '\n'.join(map(str, result)))
 
-    def test_readme_sanity(self):
-        fake_stdout = io.StringIO()  # just to ignore the output
-        fake_stderr = io.StringIO()  # will have content if there are problems
-        with open('README.rst', 'rt', encoding='utf8') as fh:
-            with patch('sys.stdout', fake_stdout):
-                with patch('sys.stderr', fake_stderr):
-                    docutils.core.publish_file(source=fh, writer=rst2html5_.HTML5Writer())
+def test_readme_sanity(mocker):
+    fake_stdout = io.StringIO()  # just to ignore the output
+    fake_stderr = io.StringIO()  # will have content if there are problems
+    with open('README.rst', 'rt', encoding='utf8') as fh:
+        mocker.patch('sys.stdout', fake_stdout)
+        mocker.patch('sys.stderr', fake_stderr)
+        docutils.core.publish_file(source=fh, writer=rst2html5_.HTML5Writer())
 
-        errors = fake_stderr.getvalue()
-        self.assertFalse(bool(errors), "There are issues!\n" + errors)
+    errors = fake_stderr.getvalue()
+    assert not bool(errors), "There are issues!\n" + errors
 
-    def test_authors_ordering(self):
-        with open('AUTHORS', 'rt', encoding='utf8') as fh:
-            authors = fh.readlines()
-        ordered_authors = sorted(authors, key=Collator().sort_key)
-        self.assertEqual(authors, ordered_authors)
+
+def test_authors_ordering():
+    with open('AUTHORS', 'rt', encoding='utf8') as fh:
+        authors = fh.readlines()
+    ordered_authors = sorted(authors, key=Collator().sort_key)
+    assert authors == ordered_authors
