f92dba3d0a85c66026ddd01c6204b2979eeee1fa
Eduardo Enriquez <eduardo.enriquez@bcc.gmbh>
2019-05-29 20:41:02 +0200

    Removed get_python_filepaths from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import os
 import shutil
 
 from pytest import fixture
@@ -10,14 +9,3 @@ def tmp_file(tmpdir_factory):
     dir_path = tmpdir_factory.mktemp("test")
     yield str(dir_path.join("testfile"))  # Converted to str to support python <3.6 versions
     shutil.rmtree(str(dir_path))
-
-
-def get_python_filepaths(roots):
-    """Helper to retrieve paths of Python files."""
-    python_paths = []
-    for root in roots:
-        for dirpath, dirnames, filenames in os.walk(root):
-            for filename in filenames:
-                if filename.endswith(".py"):
-                    python_paths.append(os.path.join(dirpath, filename))
-    return python_paths
