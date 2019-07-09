787fb10f63f707a1bfd501da7768f46ca420cce8
Eduardo Enriqez (eduzen) <eduardo.a.enriquez@gmail.com>
2019-07-09 23:43:45 +0200

    some minor changes after codereview

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,7 @@ from pytest import fixture
 def tmp_file(tmpdir_factory):
     """Fixture for a unique tmpfile for each test."""
     dir_path = tmpdir_factory.mktemp("test")
-    yield str(
-        dir_path.join("testfile")
-    )  # Converted to str to support python <3.6 versions
+    yield str(dir_path.join("testfile"))  # Converted to str to support python <3.6 versions
     shutil.rmtree(str(dir_path))
 
 
@@ -19,13 +17,13 @@ def tmp_file(tmpdir_factory):
 def create_tmpfile(tmpdir_factory):
     dir_path = tmpdir_factory.mktemp("test")
 
-    def add_content(lines=None):
+    def add_content(lines):
         """Fixture for a unique tmpfile for each test."""
         namefile = str(
             dir_path.join("testfile_{}".format(uuid.uuid4()))
         )  # Converted to str to support python <3.6 versions
         with open(namefile, "w", encoding="utf-8") as f:
-            for line in lines or []:
+            for line in lines:
                 f.write(line + "\n")
 
         return namefile
