47ccc607975e5b0954a8d5c149dc980694fa5ed0
Facundo Batista <facundo@taniquetil.com.ar>
2024-02-29 11:15:11 -0300

    Added the (forgotten) test file.

---
+++ tests/integtest.py
@@ -0,0 +1,32 @@
+# Copyright 2024 Facundo Batista, Nicolás Demarchi
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further info, check  https://github.com/PyAr/fades
+
+"""Helper to run integration tests.
+
+This is not part of the regular test suite, but a test that is used in a very specific
+way from the integration tests defined in the Github CI infrastructure.
+"""
+
+import os
+import sys
+
+
+def test_assert_python_version():
+    expected = os.environ["TEST_PYTHON_VERSION"]
+    print("========== expected")
+    vi = sys.version_info
+    current = f"{vi.major}.{vi.minor}"
+    assert current == expected
