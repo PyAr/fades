03a6eb393793977a8b6ff2a58d14bba43bf1e5a2
Facundo Batista <facundo@taniquetil.com.ar>
2024-03-04 11:13:29 -0300

    Removed debug line.

--- tests/integtest.py
+++ tests/integtest.py
@@ -26,7 +26,6 @@ import sys
 
 def test_assert_python_version():
     expected = os.environ["TEST_PYTHON_VERSION"]
-    print("========== expected")
     vi = sys.version_info
     current = f"{vi.major}.{vi.minor}"
     assert current == expected
